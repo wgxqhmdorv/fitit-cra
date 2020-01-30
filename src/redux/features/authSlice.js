727de59e983f99a73fd3a95a357fef97a2376d02
nezouse <stankiewicz1998@gmail.com>
2020-01-30 09:51:49 +0100

    Bump deps

--- src/redux/features/authSlice.js
+++ src/redux/features/authSlice.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { createSlice } from "redux-starter-kit";
+import { createSlice } from "@reduxjs/toolkit";
 import { loadRefreshToken } from "./../localStorage";
 
 const refreshToken = loadRefreshToken();
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,4 @@
-import { configureStore } from "redux-starter-kit";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
 
 export const initStore = () => configureStore({ reducer: rootReducer });
